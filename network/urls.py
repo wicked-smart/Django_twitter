159d540bb8d22729db8a39cb2628f727df64eb65
wicked-smart <marzaanvan@gmail.com>
2023-01-07 17:24:16 +0530

    Add some changes

--- network/urls.py
+++ network/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path("n/post/<int:post_id>/edit", views.edit_post, name="editpost")
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
